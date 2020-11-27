0fa793facbb7d56f9a488dc3bfeb8cd2617b8b23
Albaladejo84 <albaladejopedro84@gmail.com>
2020-11-27 09:22:25 +0100

    Clase YAML

--- src/Persona.java
+++ src/Persona.java
@@ -9,7 +9,7 @@ public class Persona {
 
 	public Persona(String nombre, int edad, String email) {
 		this.nombre = nombre;
-		this.edad = (edad>0)?edad:1;
+		this.edad = (edad > 0) ? edad : 1;
 		this.email = email;
 	}
 
@@ -17,7 +17,7 @@ public class Persona {
 	public String toString() {
 		return "Persona [nombre=" + nombre + ", edad=" + edad + ", email=" + email + "]";
 	}
-	
+
 	public String toString(String[] yaml) {
 		return "Persona [nombre=" + nombre + ", edad=" + edad + ", email=" + email + "]";
 	}
@@ -45,40 +45,61 @@ public class Persona {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
+
 	public String exportarCsv() {
-		
+
 		return nombre + ", " + edad + ", " + email;
 //		return "\"" + nombre + "\",\"" + edad + "\",\"" + email + "\" CLRF";
 	}
-	
+
 	public String exportarCsv(char separador) {
-		return "\"" + nombre + "\""  + separador + "\" " + edad + "\"" + separador + "\" "  + email + "\"";
+		return "\"" + nombre + "\"" + separador + "\" " + edad + "\"" + separador + "\" " + email + "\"";
 	}
-	
+
 	public void cargarCsv(String PersonaCsv) {
 		String[] campoStrings = PersonaCsv.split(",");
 		this.nombre = campoStrings[0];
 		setEdad(Integer.parseInt(campoStrings[1]));
 		this.email = campoStrings[2];
 	}
-	
+
 	public void cargarCsv(String PersonaCsv, String separador) {
 		String[] campoStrings = PersonaCsv.split(separador);
 		this.nombre = campoStrings[0];
 		setEdad(Integer.parseInt(campoStrings[1]));
 		this.email = campoStrings[2];
 	}
-	
+
 	public String[] exportarYAML() {
-		String[] resultado = {"nombre:" + nombre,  ", edad:" + edad, ", email:" + email};
+		String[] resultado = { "nombre:" + nombre, ", edad:" + edad, ", email:" + email };
 		return resultado;
 	}
-	
-	public void importarYAML(String[] PersonaYaml) {
-		PersonaYaml[0].split(":");
-		
-		
-	
+
+	public void importarYAML(String[] personaYaml) { // Recibe un array de YAML {"nombre: Jose Luis", "edad: 12",
+														// "email: joselito@mail.com"}
+		String[] campoNombre = personaYaml[0].split(":"); // Con split() divido cada campo del array YAML en dos campos
+															// separados por ":"
+		String[] campoEdad = personaYaml[1].split(":");
+		String[] campoEmail = personaYaml[2].split(":");
+
+		nombre = campoNombre[1].trim(); // Asidno el segundo campo de la división a los atributos del objeto Persona
+		setEdad(Integer.parseInt(campoEdad[1].trim()));
+		email = campoEmail[1].trim();
 	}
+
+	public static Persona crearPersonaConYml(String[] yml) { //Metodo que crea un objeto persona e invoca al metodo importar
+		Persona persona = new Persona();
+		persona.importarYAML(yml);
+		return persona;
+	}
+	public static Persona[] exportarPersonasYml (String[] listaYml) {
+		int numeroPersonas = listaYml.length/3;
+		Persona[] listaPersonas = new Persona[numeroPersonas];
+		for (int i = 0; i < numeroPersonas; i++) {
+			String[] personaYml = {listaYml[i*3], listaYml[i*3+1], listaYml[i*3+2]};
+			listaPersonas[i] = Persona.crearPersonaConYml(personaYml);
+		}
+		return listaPersonas;
+	}
+
 }
--- src/Principal.java
+++ src/Principal.java
@@ -3,7 +3,7 @@ public class Principal {
 	public static void main(String[] args) {
 //		
 //		Ejercicio CSV
-		
+
 //		1.- Crear una clase Persona con atributos nombre, edad, email, con:
 //		    • 	Un constructor sin argumentos que genere el nombre vacío (“”), la edad a 1, y el email 	vacío (“”). 
 //
@@ -17,33 +17,62 @@ public class Principal {
 //
 //		    • 	Crear un método cargarCsv que reciba una cadena con formato csv y cambie los datos 	de la persona
 //		2.- Crear una clase Principal con el método main() donde se probarán los métodos de la clase Persona.
-		
 
-		
 		System.out.println("Creando un CSV con los atributos de un objeto Persona");
-		Persona persona = new Persona ("Pedro", 36, "albaladejopedro84@gmail.com");
+		Persona persona = new Persona("Pedro", 36, "albaladejopedro84@gmail.com");
 		System.out.println(persona);
 		System.out.println(persona.exportarCsv());
 		System.out.println("");
-		//-----------------------------------------------------------------------------------------------------------------------------
+		// -----------------------------------------------------------------------------------------------------------------------------
 		System.out.println("Creando un objeto Persona con los atributos de un CSV");
 		String personaCsv = "Juan,18,otro@gmail.com";
 		System.out.println(personaCsv);
-		//-----------------------------------------------------------------------------------------------------------------------------
-		Persona persona2 = new Persona ();
+		// -----------------------------------------------------------------------------------------------------------------------------
+		Persona persona2 = new Persona();
 		persona2.cargarCsv(personaCsv);
 		System.out.println(persona2);
 		System.out.println("");
-		//-----------------------------------------------------------------------------------------------------------------------------
-		System.out.println("Creando un objeto Persona con los atributos de un CSV y diciendo que el caracter de separacion son dos puntos");
+		// -----------------------------------------------------------------------------------------------------------------------------
+		System.out.println(
+				"Creando un objeto Persona con los atributos de un CSV y diciendo que el caracter de separacion son dos puntos");
 		String personaCsv2 = "Jose:63:josele@gmail.com";
-		Persona persona3 = new Persona ();
+		Persona persona3 = new Persona();
 		persona3.cargarCsv(personaCsv2, ":");
 		System.out.println(persona3.exportarCsv());
 		System.out.println(persona3);
 		System.out.println("");
-		
-		System.out.println(persona.toString(persona.exportarYAML()));
+		// ---------------------------------------------------------------------------------------------------------------------------------
 
+		// 3. Crear tres metodos exportarYAML, importarYAML y crearPersonaConYAML.
+
+		System.out.println(persona.toString(persona.exportarYAML()));
+		System.out.println("");
+		System.out.println(
+				"Creando un objeto Persona con sus atributos a partir de un array de YAML (el caracter de separacion en YAML son dos puntos)");
+
+		String[] personaYaml = { "nombre: Jose Luis", "edad: 12", "email:jolesito@mail.com" }; // Arrray de YAML
+																								// recibido
+		persona.importarYAML(personaYaml);
+		System.out.println(persona.toString());
+		System.out.println("");
+
+		// ----------------------------------------------------------------------------
+		System.out.println("Creando un objeto Persona con los atributos de un array YAML");
+		String[] personaYaml2 = { "nombre: Carlos", "edad: 60", "email:carletes@mail.com" }; // Arrray de YAML recibido
+
+		System.out.println(Persona.crearPersonaConYml(personaYaml2).toString());
+		System.out.println("");
+
+		//----------------------------------------------------------------------------------
+		// 4. Recibir un YAML con los datos de varias personas y devolvr un array de personas con sus datos	
+		
+		String[] personasLista = { "nombre: Paco", "edad: 60", "email: e@mail.com",
+									"nombre: Manuel", "edad: 70", "email: m@mail.com", 
+									"nombre: Roberto", "edad: 80", "email: r@mail.com" }; 
+		
+		Persona[] listaPersonas = Persona.exportarPersonasYml(personasLista);
+		for (int i = 0; i < listaPersonas.length; i++) {
+			System.out.println(listaPersonas[i].toString());
+		}
 	}
 }
